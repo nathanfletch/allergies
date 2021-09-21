3e0c52cac9f7cc7b0b216f33d04e9d002231ea2f
nathanfletch <64681377+nathanfletch@users.noreply.github.com>
2021-09-21 14:40:18 -0700

    Initial commit

---
+++ TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+using System;
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTrackerTests
+  {
+    //static or instance method? static for now so we don't have to worry about instantiation
+    [TestMethod]
+    // Test if Equilateral: All sides are equal;
+    public void TriangleType_IsEquilateral_Equilateral()
+    {
+      Assert.AreEqual("equilateral", TriangleTracker.TriangleType(1,1,1));
+    }
+  [TestMethod]
+    public void TriangleType_IsEquilateral_NotEquilateral()
+    {
+      Console.WriteLine($"testing not equilateral");
+      
+      Assert.AreNotEqual("equilateral", TriangleTracker.TriangleType(1,1,2));
+    }
+    // Test if Isosceles: Exactly 2 sides are equal;
+     [TestMethod]
+     public void TriangleType_IsIsosceles_Isosceles()
+     {
+       Assert.AreEqual("isosceles", TriangleTracker.TriangleType(3,2,2));
+     }
+     [TestMethod]
+     public void TriangleType_IsIsosceles_NotIsosceles()
+     {
+       Assert.AreNotEqual("isosceles", TriangleTracker.TriangleType(1,3,2));
+     }
+    // Test if Scalene: No sides are equal.
+    [TestMethod]
+     public void TriangleType_IsScalene_Scalene()
+     {
+       Assert.AreEqual("scalene", TriangleTracker.TriangleType(4,3,2));
+     }
+    [TestMethod]
+     public void TriangleType_IsScalene_NotScalene()
+     {
+       Assert.AreNotEqual("scalene", TriangleTracker.TriangleType(3,3,2));
+     }
+      [TestMethod]
+     public void TriangleType_NotTriangle_NotTriangle()
+     {
+       Assert.AreEqual("not a triangle", TriangleTracker.TriangleType(3,4,8));
+     }
+    //   [TestMethod]
+    //  public void TriangleType_ErrorHandling_NotIntInput()
+    //  {
+    //    Assert.AreEqual("error", TriangleTracker.TriangleType("not", "a", "triangle"));
+    //  }
+    // Test if it's not a triangle
+    // edge cases? input: not an int, 
+  }
+}
