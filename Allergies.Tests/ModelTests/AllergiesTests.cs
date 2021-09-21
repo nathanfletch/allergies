3fc2b6849e3f52b7dff68e157925a008d0997d45
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-21 16:56:39 -0700

    WIP: adding all elements to the array
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -8,11 +8,17 @@ namespace Allergies.Tests
   [TestClass]
   public class AllergiesTests
   {
+    // [TestMethod]
+    // public void GetAllergies_AddAllergies_OneElement()
+    // {
+    //   string[] testArray = new string[1] {"eggs"};
+    //   CollectionAssert.AreEqual(testArray, Allergies.GetAllergies(1));
+    // }
     [TestMethod]
-    public void GetAllergies_AddAllergies_OneElement()
+    public void GetAllergies_AddAllergies_TwoElements()
     {
-      string[] testArray = new string[1] {"eggs"};
-      Assert.AreEqual(testArray[0], Allergies.GetAllergies(1)[0]);
+      string[] testArray = new string[2] {"eggs", "peanuts"};
+      CollectionAssert.AreEqual(testArray, Allergies.GetAllergies(2));
     }
   }
 }
--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -9,9 +9,18 @@ namespace Allergies
     {
       //need reference data hardcoded
       Dictionary<int, string> allergies = new   Dictionary<int, string>();
-      allergies.Add(1, "eggs");
-      string[] results = new string[8];
-      results[0] = allergies[n];
+      allergies.Add(1, "eggs"); //2^0
+      allergies.Add(2, "peanuts"); //2^1
+      allergies.Add(4, "shellfish"); //2^2
+      allergies.Add(8, "strawberries"); //2^3
+      allergies.Add(16, "tomatoes");
+      allergies.Add(32, "chocolate");
+      allergies.Add(64, "pollen");
+      allergies.Add(128, "cats");
+      string[] results = new string[2];
+      for (int i = 0; i <= 8; i++){
+        results[i] = allergies[Convert.ToInt32(Math.Pow(2, i))];
+      }
       return results;
     }
     
